fa6ae6f5c1073d8552a2c13b5627c20ec6d0fd81
Alan Cavalcanti <alankmc@gmail.com>
2019-02-21 09:44:27 -0800

    Updates

---
+++ precache-manifest.3d005af210af88521647425418a2a230.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7017a522804566e323e6",
+    "url": "/high-stakes-bot/static/css/main.371e3528.chunk.css"
+  },
+  {
+    "revision": "7017a522804566e323e6",
+    "url": "/high-stakes-bot/static/js/main.7017a522.chunk.js"
+  },
+  {
+    "revision": "f395368065ac2f2df48a",
+    "url": "/high-stakes-bot/static/js/runtime~main.f3953680.js"
+  },
+  {
+    "revision": "912203017420bbfb49b6",
+    "url": "/high-stakes-bot/static/js/2.91220301.chunk.js"
+  },
+  {
+    "revision": "c7163f6a109f4b4d2be7e51edd9b843a",
+    "url": "/high-stakes-bot/static/media/Flourish_borders.0.c7163f6a.svg"
+  },
+  {
+    "revision": "85ef02cf7d5940e80e870a40976e6848",
+    "url": "/high-stakes-bot/static/media/Flourish_borders.1.85ef02cf.svg"
+  },
+  {
+    "revision": "34e015a9d7adc32a0cc6c87339274118",
+    "url": "/high-stakes-bot/static/media/Flourish_borders.2.34e015a9.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/high-stakes-bot/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bf3061b2b96f732ccdafaccab63590f1",
+    "url": "/high-stakes-bot/index.html"
+  }
+];
